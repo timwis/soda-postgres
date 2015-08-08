2582845b23725f12d72871e285d0d5a5b06a7dc3
timwis <tim@timwis.com>
2015-08-07 22:28:36 -0400

    Move select processing to its own function

--- ast-to-postgresql.js
+++ ast-to-postgresql.js
@@ -5,8 +5,53 @@
 var parser = require('node-soda2-parser'),
 	_ = require('underscore');
 
+// Debug
 function inspect(obj) {
   console.log(require('util').inspect(obj, false, 10, true));
+}
+
+// SELECT processor
+var processSelect = function(columns, fields) {
+		
+	// If SELECT is *, replace with fields (so we can output geometry fields as GeoJSON)
+	if(columns === '*') {
+		columns = [];
+		fields.forEach(function(field) {
+			columns.push({
+				expr: {
+					type: 'column_ref',
+					table: '',
+					column: field.name
+				},
+				as: null
+			});
+		});
+	}
+	
+	columns.forEach(function(col) {
+		if(col.expr.type === 'column_ref') {
+			// Wrap any geometry type columns in ST_AsGeoJSON()::json
+			var field = _.findWhere(fields, {name: col.expr.column});
+			if(field && field.type === 'geometry') {
+				var fieldName = col.expr.column;
+				col.expr = {
+					type: 'raw',
+					value: 'ST_AsGeoJSON(' + col.expr.column + ')::json'
+				};
+				if(col.as === undefined || ! col.as) col.as = fieldName;
+			}
+		}
+		else if(col.expr.type === 'aggr_func') {
+			// convex_hull()
+			if(col.expr.name === 'convex_hull') {
+				col.expr = {
+					type: 'raw',
+					value: 'ST_AsGeoJSON(ST_ConvexHull(ST_Collect(' + col.expr.args.expr.column + ')))::json'
+				};
+			}
+		}
+	});
+	return columns;
 }
 
 // Recursive WHERE processor
@@ -42,45 +87,12 @@ var processWhere = function(expr) {
 }
 
 module.exports = function(ast, table, fields) {
-	// If SELECT is *, replace with fields
-	if(ast.columns === '*') {
-		ast.columns = [];
-		fields.forEach(function(field) {
-			ast.columns.push({
-				expr: {
-					type: 'column_ref',
-					table: '',
-					column: field.name
-				},
-				as: null
-			});
-		});
-	}
+	
+	// Process SELECT columns
+	ast.columns = processSelect(ast.columns, fields);
 	
 	// Add FROM table to AST
 	ast.from = [{table: table}];
-	
-	// Wrap any SELECTs of geometry type columns in ST_AsGeoJSON()
-	ast.columns.forEach(function(col) {
-		if(col.expr.type === 'column_ref') {
-			var field = _.findWhere(fields, {name: col.expr.column});
-			if(field && field.type === 'geometry') {
-				var fieldName = col.expr.column;
-				col.expr = {
-					type: 'raw',
-					value: 'ST_AsGeoJSON(' + col.expr.column + ')::json'
-				};
-				if(col.as === undefined || ! col.as) col.as = fieldName;
-			}
-		} else if(col.expr.type === 'aggr_func') {
-			if(col.expr.name === 'convex_hull') {
-				col.expr = {
-					type: 'raw',
-					value: 'ST_AsGeoJSON(ST_ConvexHull(ST_Collect(' + col.expr.args.expr.column + ')))::json'
-				};
-			}
-		}
-	});
 	
 	// Process WHERE recursively
 	if(ast.where) ast.where = processWhere(ast.where);
