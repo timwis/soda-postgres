b702dfd574fd75603864e7ecbfa776c18c014811
timwis <tim@timwis.com>
2015-08-07 20:03:04 -0400

    Added geospatial functions

--- ast-to-postgresql.js
+++ ast-to-postgresql.js
@@ -5,6 +5,10 @@
 var parser = require('node-soda2-parser'),
 	_ = require('underscore');
 
+function inspect(obj) {
+  console.log(require('util').inspect(obj, false, 10, true));
+}
+
 var buildAstColumn = function(column, alias, table) {
 	return {
 		expr: {
@@ -14,6 +18,42 @@ var buildAstColumn = function(column, alias, table) {
 		},
 		as: alias || null
 	};
+};
+
+var buildAstFunc = function(name, expr) {
+	return {
+		type: 'aggr_func',
+		name: name,
+		args: { expr: expr }
+	}
+}
+
+// Recursive WHERE processor
+var processWhere = function(expr) {
+	// If AND or OR, recurse
+	if(expr.type === 'binary_expr' && (expr.operator === 'AND' || expr.operator === 'OR')) {
+		expr.left = processWhere(expr.left);
+		expr.right = processWhere(expr.right);
+	} else if(expr.type === 'function') {
+		// within_box()
+		if(expr.name === 'within_box') {
+			var field = expr.args.value.shift().column,
+				points = _.pluck(expr.args.value, 'value');
+			expr.type = 'raw';
+			expr.value = field + ' && ST_MakeEnvelope(' + points.join(', ') + ', 4326)';
+		}
+		// within_circle()
+		else if(expr.name === 'within_circle') {
+			expr.name = 'ST_Point_Inside_Circle';
+			expr.args.value[expr.args.value.length-1].value *= 0.00001; // convert degrees to meters
+		}
+		// within_polygon()
+		else if(expr.name === 'within_polygon') {
+			expr.name = 'ST_Within';
+			expr.args.value[1] = buildAstFunc('ST_GeometryFromText', expr.args.value[1]);
+		}
+	}
+	return expr;
 }
 
 module.exports = function(ast, table, fields) {
@@ -34,11 +74,20 @@ module.exports = function(ast, table, fields) {
 			var field = _.findWhere(fields, {name: col.expr.column});
 			if(field && field.type === 'geometry') {
 				var fieldName = col.expr.column;
-				col.expr = {type: 'aggr_func', name: 'ST_AsGeoJSON', args: { expr: col.expr } };
+				//col.expr = {type: 'aggr_func', name: 'ST_AsGeoJSON', args: { expr: col.expr } };
+				col.expr = buildAstFunc('ST_AsGeoJSON', col.expr);
 				if(col.as === undefined || ! col.as) col.as = fieldName;
+			}
+		} else if(col.expr.type === 'aggr_func') {
+			if(col.expr.name === 'convex_hull') {
+				col.expr.name = 'ST_AsGeoJSON';
+				col.expr.args = {expr: buildAstFunc('ST_ConvexHull', buildAstFunc('ST_Collect', col.expr.args.expr))};
 			}
 		}
 	});
+	
+	// Process WHERE recursively
+	if(ast.where) ast.where = processWhere(ast.where);
 	
 	// Convert AST back to SQL
 	var sql = parser.stringify.parse(ast);
--- test/test.js
+++ test/test.js
@@ -1,23 +1,70 @@
 var should = require('should'),
 	parser = require('node-soda2-parser'),
 	astToPsql = require('../ast-to-postgresql'),
-  fields = require('./fields');
+  fields = require('./data/fields');
 
 function inspect(obj) {
   console.log(require('util').inspect(obj, false, 10, true));
 }
 
-describe('select',function(){
+function getPsql(soda) {
+  var ast = parser.parse(soda);
+  return astToPsql(ast, 'sites', fields);
+}
+
+describe('select',function() {
 
   it('multiple fields', function() {
-    var ast = parser.parse('$select=foo, bar'),
-			sql = astToPsql(ast, 'sites', fields)
+    var ast = parser.parse('$select=foo, bar')
+    var sql = astToPsql(ast, 'sites', fields)
     sql.should.eql('SELECT foo, bar FROM sites')
-  });
+  })
   
   it('wildcard', function() {
-    var ast = parser.parse('$select=*'),
-      sql = astToPsql(ast, 'sites', fields)
+    var ast = parser.parse('$select=*')
+    var sql = astToPsql(ast, 'sites', fields)
     sql.should.eql('SELECT id, date, time, address, location, city, state, zip_code, screening_type, contact_information, phone_number, ST_AsGeoJSON(geom)::json AS geom FROM sites')
   })
-});
+  
+  // Fails because of regex that appends ::json not being greedy
+  it('convex hull', function() {
+    var ast = parser.parse('$select=convex_hull(geom)')
+    var sql = astToPsql(ast, 'sites', fields)
+    sql.should.eql('SELECT ST_AsGeoJSON(ST_ConvexHull(ST_Collect(geom)))::json FROM sites')
+  })
+  
+})
+
+describe('where', function() {
+  
+  it('operator', function() {
+    var ast = parser.parse('$select=foo&$where=foo < 3')
+    var sql = astToPsql(ast, 'sites', fields)
+    sql.should.eql('SELECT foo FROM sites WHERE foo < 3')
+  })
+  
+  it('within box', function() {
+    var ast = parser.parse('$select=foo&$where=within_box(geom, 47.5, -122.3, 47.5, -122.3)')
+    var sql = astToPsql(ast, 'sites', fields)
+    sql.should.eql('SELECT foo FROM sites WHERE geom && ST_MakeEnvelope(47.5, -122.3, 47.5, -122.3, 4326)')
+  })
+  
+  it('within circle', function() {
+    var ast = parser.parse('$select=foo&$where=within_circle(geom, 47.59815, -122.33454, 500)')
+    var sql = astToPsql(ast, 'sites', fields)
+    sql.should.eql('SELECT foo FROM sites WHERE ST_Point_Inside_Circle(geom, 47.59815, -122.33454, 0.005)')
+  })
+  
+  it('within polygon', function() {
+    var ast = parser.parse('$select=foo&$where=within_polygon(geom, \'MULTIPOLYGON (((-87.637714 41.887275, -87.613681 41.886892, -87.625526 41.871555, -87.637714 41.887275)))\')')
+    var sql = astToPsql(ast, 'sites', fields)
+    sql.should.eql('SELECT foo FROM sites WHERE ST_Within(geom, ST_GeometryFromText(\'MULTIPOLYGON (((-87.637714 41.887275, -87.613681 41.886892, -87.625526 41.871555, -87.637714 41.887275)))\'))')
+  })
+  
+  it('multiple', function() {
+    var ast = parser.parse('$select=foo&$where=foo < 2 and within_box(geom, 47.5, -122.3, 47.5, -122.3) AND bar = \'test\'')
+    var sql = astToPsql(ast, 'sites', fields)
+    sql.should.eql('SELECT foo FROM sites WHERE foo < 2 AND geom && ST_MakeEnvelope(47.5, -122.3, 47.5, -122.3, 4326) AND bar = \'test\'')
+  })
+  
+})
