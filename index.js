21299cef66862a7c9743c99d454c927b4393003c
timwis <tim@timwis.com>
2015-08-08 14:32:45 -0400

    Moved processing to libs, other logic to index.js

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 var parser = require('../node-soda2-parser'),
 	restify = require('restify'),
 	_ = require('underscore'),
-	astToPsql = require('./ast-to-postgresql'),
-	anyDB = require('any-db-postgres');
+	anyDB = require('any-db-postgres'),
+	processWhere = require('./lib/where'),
+	processSelect = require('./lib/select');
 require('dotenv').load();
-var inspect = function(data) { console.log(require('util').inspect(data, false, 10, true)); };
 
-var conn = anyDB.createConnection(process.env.DATABASE_URL);
-	
-var server = restify.createServer();
+var conn = anyDB.createConnection(process.env.DATABASE_URL),
+	tables = {},
+	server = restify.createServer();
 server.use(restify.queryParser());
-
-var tables = {};
 
 server.get('/resource/:table', function(req, res, next) {
 	// If table doesn't exist, send error
 	if(tables[req.params.table] === undefined) {
 		return res.send(404);
 	}
-	var ast = parser.parse(req.query),
-		sql = astToPsql(ast, req.params.table, tables[req.params.table]);
 	
-	// Query database
-	//console.log(req.query, sql);
+	// Parse query into AST
+	var ast = parser.parse(req.query);
+	
+	// Process SELECT columns
+	ast.columns = processSelect(ast.columns, tables[req.params.table]);
+	
+	// Add FROM table to AST
+	ast.from = [{table: req.params.table}];
+	
+	// Process WHERE recursively
+	if(ast.where) ast.where = processWhere(ast.where);
+	
+	// Convert AST back to SQL
+	var sql = parser.stringify.parse(ast);
+	
+	// Query database with SQL
 	conn.query(sql, function(err, result) {
 		if(err) console.error(err);
 		res.json(result.rows);
