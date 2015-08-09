7326a0f96796f814e7d56bf2d7c6c2d07d134fc6
timwis <tim@timwis.com>
2015-08-08 21:09:09 -0400

    dotenv silent

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var parser = require('node-soda2-parser'),
 	anyDB = require('any-db-postgres'),
 	processWhere = require('./lib/where'),
 	processSelect = require('./lib/select');
-require('dotenv').load();
+require('dotenv').load({silent: true});
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', 
